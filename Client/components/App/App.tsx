08dc9bc5104b30453c585a27ab567a160729640f
Tem Pearse <tem.pearse01@gmail.com>
2023-08-06 18:15:39 +1200

    add index.tsx, Header and App

---
+++ Client/components/App/App.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom'
+import Header from './Header/Header'
+
+function App() {
+  return (
+    <div>
+      <section className="main">
+        <div className="left-top">
+          <Header />
+        </div>
+      </section>
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
--- Client/index.tsx
+++ Client/index.tsx
@@ -1,1 +1,16 @@
 import { createRoot } from 'react-dom/client'
+import { Suspense, lazy, useEffect } from 'react'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import App from './components/App/App'
+
+export const routes = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<App />}></Route>
+  </Route>
+)
