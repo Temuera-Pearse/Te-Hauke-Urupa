26551aa36d7c8be5efb67a4e30c8528c5e5d6b5e
Tem Pearse <tem.pearse01@gmail.com>
2023-08-13 18:03:05 +1200

    added home page password box

--- Server/server.ts
+++ Server/server.ts
@@ -1,19 +1,21 @@
 import express from 'express'
-import { join } from 'node:path'
-import axios from 'axios'
+import * as Path from 'node:path'
+
+import profileRoutes from './routes/profiles'
 
 const server = express()
 
 server.use(express.json())
-server.use(express.static(join(__dirname, 'public')))
 
-server.get(async (req, res) => {
-  try {
-    const response = await axios.get('https://api.example.com/data')
-    res.json(response.data)
-  } catch (error) {
-    res.status(500).json({ error: 'An error occurred' })
-  }
-})
+server.use('/api/v1/profiles', profileRoutes)
+server.use('/api/v1/*', (req, res) => res.sendStatus(404))
+
+if (process.env.NODE.ENV === 'production') {
+  server.use('/assets', express.static(Path.resolve(__dirname, '../assets')))
+
+  server.get('*', (req, res) => {
+    res.sendfile(Path.resolve(__dirname, '../index.html'))
+  })
+}
 
 export default server
