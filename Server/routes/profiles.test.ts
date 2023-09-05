33e28a9ce7854a9f4b8f63a2edc1f7a425252588
Tem Pearse <tem.pearse01@gmail.com>
2023-09-05 13:34:45 +1200

    3 route tests made

--- Server/routes/profiles.test.ts
+++ Server/routes/profiles.test.ts
@@ -4,39 +4,56 @@ import server from '../server'
 import { getProfiles } from '../db/db'
 import * as db from '../db/db'
 import { proBackSchema } from '../../models/profile'
+import { response } from 'express'
 
 vi.mock('../db/db')
 
 describe('GET /api/v1/profiles', () => {
-  it('should return 200 with an array', async () => {
-    const fakeProfile: proBackSchema[] = [
-      {
-        id: 1,
-        profile_picture: 'pic.jpeg',
-        family_name: 'fake_family_name',
-        given_name: 'fake_given_name',
-        address: 'fake_addy',
-        gender: 'fake_gender',
-        Occupation: 'fake_job',
-        age: 'fake_age',
-        birthDate: 'fake_birthday',
-        deathDate: 'fake_death',
-        intermentDate: 'fake_interment',
-        intermentType: 'fake_interment_type',
-        placement: 'fake_placment',
-        depth: 'fake_depth',
-        purchaseDate: 'fake_purchase',
-        funeralDirector: 'fake_director',
-        notes: '---',
-        headstonePermitNumber: 'fake_permit_number',
-        inscriptions: 'fake',
-        headstonePicture: '',
-        headstoneLocationPic: '',
-      },
-    ]
+  const fakeProfile: proBackSchema[] = [
+    {
+      id: 1,
+      profile_picture: 'pic.jpeg',
+      family_name: 'fake_family_name',
+      given_name: 'fake_given_name',
+      address: 'fake_addy',
+      gender: 'fake_gender',
+      Occupation: 'fake_job',
+      age: 'fake_age',
+      birthDate: 'fake_birthday',
+      deathDate: 'fake_death',
+      intermentDate: 'fake_interment',
+      intermentType: 'fake_interment_type',
+      placement: 'fake_placment',
+      depth: 'fake_depth',
+      purchaseDate: 'fake_purchase',
+      funeralDirector: 'fake_director',
+      notes: '---',
+      headstonePermitNumber: 'fake_permit_number',
+      inscriptions: 'fake',
+      headstonePicture: '',
+      headstoneLocationPic: '',
+    },
+  ]
 
+  it('should return 200 with an array', async () => {
     vi.mocked(db.getProfiles).mockResolvedValue(fakeProfile)
     const response = await request(server).get('/api/v1/profiles')
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual(fakeProfile)
+  })
+
+  it('should return profile when searched', async () => {
+    vi.mocked(db.getProfileBySearch).mockResolvedValue(fakeProfile)
+    const response = await request(server)
+      .get('/api/v1/profiles')
+      .query({ searchTerm: 'fake_given_name' })
+    expect(response.status).toBe(200)
+    expect(response.body).to.deep.equal(fakeProfile)
+  })
+
+  it('should return a profile by id', async () => {
+    vi.mocked(db.getProfilesById).mockResolvedValue(fakeProfile)
+    const response = await request(server).get(`/api/v1/profiles/${1}`)
     expect(response.status).toBe(200)
     expect(response.body).toEqual(fakeProfile)
   })
